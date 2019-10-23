514cfbc8f20967a806aa324b4d78f272b91bef47
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-10-23 14:17:50 +0200

    Update showtasks.bat

--- showtasks.bat
+++ showtasks.bat
@@ -6,7 +6,7 @@ goto fail
 
 
 :browser
-explorer "http://localhost:8080/crud/v1/task/getTasks"
+explorer "http://localhost:8080/crud/v1/task_crud/tasks/getTasks"
 timeout 30
 if "%ERRORLEVEL%" == "0" goto end
 echo.
