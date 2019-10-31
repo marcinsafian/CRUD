117ab5c5c92de880c2213286e6e1e8d1d5d97a8e
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-10-31 16:02:13 +0100

    update

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -22,8 +22,25 @@ public class TrelloController {
         List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
 
         trelloBoards.stream()
-                .filter(trelloBoardDto -> trelloBoardDto.getName().equals("Kodilla"))
+                .filter(trelloBoardDto -> trelloBoardDto.getName().equals("Kodilla Application"))
                 .forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
+    trelloBoards.forEach(trelloBoardDto -> {trelloBoardDto.getLists().forEach(trelloList ->
+            System.out.println(trelloList.getName() +"-"+ trelloList.getId() + "-" + trelloList.isClosed()));});
+        // GET request
+      /*
 
+        trelloBoards.forEach(trelloBoardDto -> {
+
+            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
+
+            System.out.println("This board contains lists: ");
+
+            trelloBoardDto.getLists().forEach(trelloList ->
+                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
+
+        });
+
+       */
     }
+
 }
