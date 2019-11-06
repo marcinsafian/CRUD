6a94a321597cf37577090dfac8c5e20baba569a5
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-11-06 10:32:43 +0100

    modul 23.2_select only one pack

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -20,7 +20,7 @@ public class CoreConfiguration implements WebMvcConfigurer {
     }
     @Bean public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select() .apis(RequestHandlerSelectors.any())
+                .select() .apis(RequestHandlerSelectors.basePackage("com.crud.tasks.controller"))
                 .paths(PathSelectors.any()) .build();
     }
     @Override
