80a4277ad36017bbac68c04c4452442ddd92a89d
Marcin Szymanski <marcin.szymanski.poczta@gmail.com>
2019-11-11 17:30:27 +0100

    update

--- src/main/java/com/crud/tasks/domain/Mail.java
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -2,7 +2,6 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
@@ -10,5 +9,9 @@ public class Mail {
     private String mailTo;
     private String subject;
     private String message;
+    private String toCc;
 
+
+    public Mail(String mailTo, String subject, String message) {
+    }
 }
